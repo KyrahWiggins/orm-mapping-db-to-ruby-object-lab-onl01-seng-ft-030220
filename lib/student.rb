d552a99cec2ff9026b281d1721c1580d9213a27f
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-09-04 19:23:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,44 +1,44 @@
 attr_accessor :id, :name, :grade
 
- 
+
      def self.new_from_db(row)
      # create a new Student object given a row from the database		     new_student = self.new
      new_student.id = row[0]
      new_student.name = row[1]
      new_student.grade = row[2]
      new_student
-   end		   
+   end
 
- 
-    def self.all	
-          SELECT * 
+
+    def self.all
+          SELECT *
      FROM students
      SQL
      DB[:conn].execute(sql).map do |row|
        self.new_from_db(row)
      end
-   end		   
+   end
 
- 
+
    def self.find_by_name(name)
      # find the student in the database given a name		     sql = <<-SQL
-     # return a new instance of the Student class		     SELECT * 
-     FROM students 
-     WHERE name = ? 
+     # return a new instance of the Student class		     SELECT *
+     FROM students
+     WHERE name = ?
      LIMIT 1
      SQL
      DB[:conn].execute(sql, name).map do |row|
        self.new_from_db(row)
      end.first
-   end		   
+   end
 
    def save
           sql = <<-SQL
         INSERT INTO students (name, grade)
   SQL
-      DB[:conn].execute(sql, self.name, self.grade)		   
+      DB[:conn].execute(sql, self.name, self.grade)
     end
-    		  
+
   	  def self.create_table
 	    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
@@ -46,18 +46,18 @@ attr_accessor :id, :name, :grade
             name TEXT,
   		      grade TEXT
         )
-      SQL		    
+      SQL
 	    DB[:conn].execute(sql)
-    end		
-    
+    end
+
     def self.drop_table
      sql = "DROP TABLE IF EXISTS students"
-     DB[:conn].execute(sql)		    
-   end		   
+     DB[:conn].execute(sql)
+   end
    def self.students_below_12th_grade
      sql = <<-SQL
-     SELECT * 
-     FROM students 
+     SELECT *
+     FROM students
      WHERE grade < 12
      SQL
      DB[:conn].execute(sql).map do |row|
@@ -90,5 +90,3 @@ attr_accessor :id, :name, :grade
    end
 
  
-
-    
